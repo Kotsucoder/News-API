731a22d2672ffea8c60dd3fb3b76d127a69b5406
Screenbones <whoami@kotsu.red>
2024-03-28 15:37:49 -0700

    Five Program Refinements

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import requests
 import os
 import smtplib, ssl
 
+topic = "apple computer"
 api_key = os.getenv("NEWSAPI")
-url = f"https://newsapi.org/v2/everything?q=tesla&from=2024-02-28&\
-    sortBy=publishedAt&apiKey={api_key}"
+url = f"https://newsapi.org/v2/everything?" \
+    f"q={topic}&" \
+    f"sortBy=publishedAt&" \
+    f"apiKey={api_key}&" \
+    f"language=en"
 
 host = "smtp.gmail.com"
 port = 465
@@ -16,11 +20,12 @@ context = ssl.create_default_context()
 
 request = requests.get(url)
 content = request.json()
-email = ""
-for article in content["articles"]:
+email = "Subject: Today's news\n"
+for article in content["articles"][:20]:
     if article["title"] is not None and article["description"] is not None:
         email = email + article["title"] + '\n'
         email = email + article["description"] + '\n'
+        email = email + article["url"] + '\n'
         email = email + '\n'
 
 with smtplib.SMTP_SSL(host, port, context=context) as server:
